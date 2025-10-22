b6f7e188f7779c7eea0eabff3e1e7de948d32975
vaki <antonio.valec04@gmail.com>
2025-10-22 19:16:11 +0200

    funkcionalan logim, radi redirect na stranicu /hello nakon prijave

--- Login/src/main/java/com/oauth/login/LoginApplication.java
+++ Login/src/main/java/com/oauth/login/LoginApplication.java
@@ -3,7 +3,7 @@ package com.oauth.login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
 public class LoginApplication {
 
     public static void main(String[] args) {
