e381309720d91c3d9651b14b3943b82a2ec7bafd
Antonio Valec <antonio.valec@fer.unizg.hr>
2025-10-25 17:43:38 +0200

    kreiran modul na kojem ce biti backend

---
+++ ServisApp/src/main/java/com/havana/servisapp/ServisAppApplication.java
@@ -0,0 +1,13 @@
+package com.havana.servisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServisAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServisAppApplication.class, args);
+    }
+
+}
---
+++ ServisApp/src/test/java/com/havana/servisapp/ServisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.havana.servisapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServisAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
