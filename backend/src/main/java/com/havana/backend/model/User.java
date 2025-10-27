c8dbbbe248c08602e47647f272b785ed705b5871
Antonio Valec <antonio.valec@fer.unizg.hr>
2025-10-27 21:02:47 +0100

    dodana anotacija za stupce u USER

--- backend/src/main/java/com/havana/backend/model/User.java
+++ backend/src/main/java/com/havana/backend/model/User.java
@@ -16,8 +16,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column
     private String pictureUrl;
 }
