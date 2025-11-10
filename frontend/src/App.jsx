9c18c6dab3b3c975a61ebcb2ad3516caa7768b97
Kristian Vranješ <kv119125308@fer.hr>
2025-11-10 22:21:51 +0100

    Add appointments UI and supporting frontend config (copied from kristian)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Services from './pages/Services/Services'
 import Appointments from './pages/Appointments/Appointments'
 import Footer from './components/Footer/Footer'
-import Contact from './pages/contact/contact'
-
-
+import Contact from './pages/Contact/Contact'
 
 export default function App() {
   return (
-    <>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Navbar />
-      <div style={{padding:12}}>
+
+      <Box component="main" sx={{ flex: 1, p: 2 }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/appointments" element={<Appointments />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </div>
+      </Box>
+
       <Footer />
-    </>
+    </Box>
   )
 }
