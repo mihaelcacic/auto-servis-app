856c8c0db8be597ba30460b45edced847ce52867
Antonio Valec <antonio.valec@fer.unizg.hr>
2025-11-11 23:46:13 +0100

    Update SecurityConfig.java

--- backend/src/main/java/com/havana/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/havana/backend/configuration/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/api/**").permitAll()
+                        .requestMatchers("/api/usluge", "/api/serviseri", "/api/marke", "/api/model/*", "/api/zamjenska-vozila/slobodna", "/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
