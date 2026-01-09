baacd1f3f4023cf674ae30c53ef6db28b6d4f8f4
Antonio Valec <antonio.valec@fer.unizg.hr>
2026-01-09 18:19:43 +0100

    popravljen ngnix

--- frontend/src/config/env.js
+++ frontend/src/config/env.js
@@ -1,12 +1,2 @@
-function requireEnv(name) {
-    const value = import.meta.env[name];
 
-    if (!value) {
-        throw new Error(`Missing environment variable: ${name}`);
-    }
-
-    return value;
-}
-
-export const BACKEND_URL = requireEnv('VITE_BACKEND_URL');
-export const FRONTEND_URL = requireEnv('VITE_FRONTEND_URL');
+export const BACKEND_URL = "http://localhost";
