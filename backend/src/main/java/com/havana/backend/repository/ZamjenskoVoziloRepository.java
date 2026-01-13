15d1b8677b58f839f9e2f3a57b6bc016a1522758
Antonio Valec <antonio.valec@fer.unizg.hr>
2026-01-13 20:34:02 +0100

    Update ZamjenskoVoziloRepository.java

--- backend/src/main/java/com/havana/backend/repository/ZamjenskoVoziloRepository.java
+++ backend/src/main/java/com/havana/backend/repository/ZamjenskoVoziloRepository.java
@@ -18,8 +18,8 @@ public interface ZamjenskoVoziloRepository extends JpaRepository<ZamjenskoVozilo
 
     @Query("""
         SELECT z FROM ZamjenskoVozilo z
-        WHERE z.datumPreuzimanja IS NULL
-        OR z.datumVracanja IS NOT NULL
+        WHERE (z.datumPreuzimanja IS NULL AND z.datumVracanja IS NULL)
+        OR (z.datumPreuzimanja IS NOT NULL AND z.datumVracanja IS NOT NULL)
     """)
     List<ZamjenskoVozilo> findSlobodna();
 }
